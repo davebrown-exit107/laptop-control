e5def6615b63904ade43f91c6547dd00a7614c6e
Dave Brown <dave.brown@exit107.com>
2021-11-14 00:03:03 -0700

    Cleaning up boilerplate comments

--- cmd/root.go
+++ cmd/root.go
@@ -41,9 +41,6 @@ laptop-control uses dbus and other means to control your laptop's
 hardware aspects such as volume, screen brightness, and keyboard
 brightness. It's designed to be called by scripts to ease the 
 configuration of swaywm.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -54,15 +51,7 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $XDG_CONFIG_HOME/laptop-control.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
